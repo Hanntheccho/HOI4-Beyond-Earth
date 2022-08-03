db261bae75f168d94e93373d32a42eb74a1d01ef
Hanntheccho <jmgp0411@gmail.com>
2022-08-04 00:18:20 +0200

    update

--- common/defines/mod_defines.lua
+++ common/defines/mod_defines.lua
@@ -40,7 +40,6 @@ NDefines.NResistance.RESISTANCE_GROWTH_BASE = 0.4                -- Vanilla: 0.2
 NDefines.NProduction.BASE_FACTORY_SPEED = 8                    -- Vanilla: 5
 NDefines.NProduction.BASE_FACTORY_SPEED_MIL = 6                 -- Vanilla: 4.5
 NDefines.NProduction.BASE_FACTORY_SPEED_NAV = 4                   -- Vanilla: 2.5
-NDefines.NAI.MAX_DISTANCE_NALAV_INVASION = 400.0                   -- Vanilla: 
 
 NDefines.NDiplomacy.BASE_IMPROVE_RELATION_DIFFERENT_IDEOLOGY_GROUP_MAINTAIN_COST = 0.2
 
@@ -48,3 +47,15 @@ NDefines.NDiplomacy.WARGOAL_JUSTIFY_TENSION_FROM_PRODUCTION = 0
 
 NDefines.NOperatives.AGENCY_CREATION_FACTORIES = 1000101
 
+NDefines.NAI.ENEMY_HOME_AREA_RATIO_TO_DISABLE_INVASIONS = 1.0   -- Vanilla: 0.3
+NDefines.NAI.ENEMY_NAVY_STRENGTH_DONT_BOTHER = 10.0     -- Vanilla: 1.9
+NDefines.NAI.RELATIVE_STRENGTH_TO_INVADE = 0.01           -- Vanilla: 0.09
+NDefines.NAI.RELATIVE_STRENGTH_TO_INVADE_DEFENSIVE = 0.01   -- Vanilla: 0.1
+
+NDefines.NAI.MAX_DISTANCE_NALAV_INVASION = 400.0                   -- Vanilla: 200.0
+NDefines.NAI.MAX_UNIT_RATIO_FOR_INVASIONS = 1.0               -- Vanilla: 0.3
+NDefines.NAI.MIN_UNIT_RATIO_FOR_INVASIONS = 1.0              -- Vanilla: 0.1
+NDefines.NAI.MIN_INVASION_PLAN_VALUE_TO_EXECUTE = 0.02          -- Vanilla: 0.2
+NDefines.NAI.MIN_INVASION_ORG_FACTOR_TO_EXECUTE = 0.25               -- Vanilla: 0.75
+
+NDefines.NAI.FAILED_INVASION_AVOID_DURATION = 90           -- Vanilla: 135
