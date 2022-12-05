dec2e49b7ea01d7b7229585d38a53d43614a7745
Hanntheccho <jmgp0411@gmail.com>
2022-12-06 00:26:00 +0100

    update

--- common/defines/mod_graphics.lua
+++ common/defines/mod_graphics.lua
@@ -1,6 +1,8 @@
 NDefines_Graphics.NFrontend.CAMERA_MIN_HEIGHT = 25.0
 NDefines_Graphics.NFrontend.CAMERA_MAX_HEIGHT = 2800.0
 
+NDefines_Graphics.NGraphics.TOOLTIP_DELAYED_DELAY = 0.4
+
 NDefines_Graphics.NSound.HEIGHT_SOUND_MIN_ALTITUDE = 30.0
 NDefines_Graphics.NSound.HEIGHT_SOUND_MAX_ALTITUDE = 300.0
 NDefines_Graphics.NSound.HEIGHT_SOUND_MIN_VOLUME = 0.7
