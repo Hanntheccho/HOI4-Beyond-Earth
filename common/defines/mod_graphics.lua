01631356bf72975c2581724e5bab8638c965e7cb
Hanntheccho <jmgp0411@gmail.com>
2023-01-22 12:45:38 +0100

    update, fixes

--- common/defines/mod_graphics.lua
+++ common/defines/mod_graphics.lua
@@ -63,5 +63,3 @@ NDefines_Graphics.NGraphics.RAILWAY_BRIDGE_GAP_WIDTH = 0.0			-- Railways will ha
 NDefines_Graphics.NGraphics.RAILWAY_BRIDGE_GAP_LARGE_WIDTH = 0.0			-- Railways will have gaps of this length for large bridges
 NDefines_Graphics.NGraphics.TRAIN_MAP_SPEED = 4.0							-- Trains will move at this relative speed. This has no gameplay implications. Changing this value (originally 4.0) may cause audio effects to lose sync with animation.
 NDefines_Graphics.NGraphics.TUNNELBANA_TIMETABLE = { 4000, 6000 }	
-
-NDefines_Graphics.NInterface.MILITARY_FACTORIES_SCALE = { 1, 1, 1}
