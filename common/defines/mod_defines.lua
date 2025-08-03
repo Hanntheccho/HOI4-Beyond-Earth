1a41657bf10c2686f2ee7c02c6cd8788b36035cc
Hanntheccho <jmgp0411@gmail.com>
2025-08-03 04:31:49 +0200

    define tweaks

--- common/defines/mod_defines.lua
+++ common/defines/mod_defines.lua
@@ -1,4 +1,4 @@
-NDefines.NGame.START_DATE = "2199.1.1.1"
+NDefines.NGame.START_DATE = "2000.1.1.1"
 NDefines.NGame.END_DATE = "2250.1.1.1"
 NDefines.NGraphics.COUNTRY_FLAG_LARGE_STRIPE_MAX_HEIGHT = 24000        -- VANILLA: 8192
 NDefines.NGraphics.COUNTRY_FLAG_SMALL_TEX_MAX_SIZE = 512            -- VANILLA: 64
@@ -10,9 +10,9 @@ NDefines.NGame.FUEL_RESOURCE = "energy"
 
 NDefines.NBuildings.MAX_SHARED_SLOTS = 20
 
-NDefines.NAI.BUILDING_TARGETS_BUILDING_PRIORITIES = { 'civilian_complex', 'military_complex' }      -- ?
-
-NDefines.NPolitics.BASE_POLITICAL_POWER_INCREASE = 1  -- Vanilla: 2
+NDefines.NPolitics.BASE_POLITICAL_POWER_INCREASE = 1    -- Vanilla: 2
+NDefines.NCountry.POLITICAL_POWER_UPPER_CAP = 1000.0    -- Vanilla: 2000.0
+NDefines.NCountry.POLITICAL_POWER_LOWER_CAP = -999.0    -- Vanilla: -500.0
 
 NDefines.NDiplomacy.TENSION_STATE_VALUE = 0                            -- Vanilla: 2
 NDefines.NDiplomacy.TENSION_NO_CB_WAR = 0                            -- Vanilla: 15
@@ -38,44 +38,19 @@ NDefines.NDiplomacy.TENSION_RESOURCE_RIGHTS = 0                           -- Van
 NDefines.NDiplomacy.TENSION_DISMANTLE_INDUSTRY = 0                           -- Vanilla: 0.25
 NDefines.NDiplomacy.TENSION_CAPITULATE = 0                           -- Vanilla: 0.75
 
-NDefines.NDiplomacy.AUTONOMY_LEVEL_CHANGE_PP_ANNEX = 6969
-NDefines.NDiplomacy.AUTONOMY_LEVEL_CHANGE_PP_FREE = 6969
-
 NDefines.NCountry.POPULATION_YEARLY_GROWTH_BASE = 0.0005           -- Vanilla: 0.015
 NDefines.NCountry.SUPPLY_BASE_MULT = 10                          -- Vanilla: 1
-
-NDefines.NCountry.POLITICAL_POWER_UPPER_CAP = 1000.0
-NDefines.NCountry.POLITICAL_POWER_LOWER_CAP = -999.0
-
-NDefines.NCountry.SCORCHED_EARTH_STATE_COST = 6969
-
-NDefines.NCountry.WAR_SUPPORT_TENSION_IMPACT = 0.0
-
-NDefines.NDiplomacy.WARGOAL_WORLD_TENSION_REDUCTION = 13
 
 NDefines.NOperatives.AGENCY_CREATION_FACTORIES = 1000101
 
 --NDefines.NCountry.WEEKLY_STABILITY_GAIN = 0.0
 --NDefines.NCountry.WEEKLY_WAR_SUPPORT_GAIN = 0.0
 
-NDefines.NProduction.BASE_FACTORY_SPEED = 1
-NDefines.NProduction.BASE_FACTORY_SPEED_MIL = 1
-NDefines.NProduction.BASE_FACTORY_SPEED_NAV = 1
-
-NDefines.NMilitary.BASE_NIGHT_ATTACK_PENALTY = 0.0
-
 NDefines.NMilitary.GENERATE_AI_DIV_COMMAND_HISTORY_ENTRIES = false
 
 NDefines.NCountry.BASE_COMMAND_POWER_GAIN = -1.0      -- Vanilla: 0.4
 
-NDefines.NPolitics.ARMY_LEADER_COST = 100
-NDefines.NPolitics.NAVY_LEADER_COST = 100
-
 NDefines.NCountry.DAYS_OF_WAR_BEFORE_SURRENDER = 1  -- Vanilla: 7
-
-NDefines.NAir.DAY_NIGHT_COVERAGE_FACTOR = 1.0
-NDefines.NAir.DETECT_CHANCE_FROM_NIGHT = 0.0
-NDefines.NAir.CAS_NIGHT_ATTACK_FACTOR = 0.0
 
 --NDefines.NFocus.FOCUS_PROGRESS_PEACE = 1
 --NDefines.NFocus.FOCUS_PROGRESS_WAR = 1
@@ -91,42 +66,6 @@ NDefines.NSupply.VP_TO_SUPPLY_BASE = 0.3
 NDefines.NSupply.VP_TO_SUPPLY_BONUS_CONVERSION = 0.07
 NDefines.NSupply.AVAILABLE_MANPOWER_STATE_SUPPLY = 0.3
 
---NDefines.NSupply.SUPPLY_HUB_FULL_MOTORIZATION_BONUS = 0.0
---NDefines.NSupply.SUPPLY_HUB_FULL_MOTORIZATION_TRUCK_COST = 0.0
-
---NDefines.NAI.UPDATE_SUPPLY_MOTORIZATION_FREQUENCY_HOURS = 69696969696969 --would -1 disable the AI?
-
-NDefines.NIntel.CIVILIAN_INTEL_NEEDED_TO_SHOW_FOCUS_TREE = 0.0
-NDefines.NIntel.CIVILIAN_INTEL_NEEDED_TO_SHOW_CURRENT_FOCUS = 0.0
-NDefines.NIntel.CIVILIAN_INTEL_NEEDED_TO_SHOW_CURRENT_FOCUS_PROGRESS = 0.0
-
-NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 0
-NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 0
-NDefines.NMilitary.MAX_AIR_EXPERIENCE = 0
-
---NDefines.NMilitary.CORPS_COMMANDER_DIVISIONS_CAP = -1
---NDefines.NMilitary.CORPS_COMMANDER_ARMIES_CAP = -1
-NDefines.NMilitary.FIELD_MARSHAL_DIVISIONS_CAP = -1
-NDefines.NMilitary.FIELD_MARSHAL_ARMIES_CAP = -1
-
-NDefines.NMilitary.FIELD_MARSHAL_ARMY_BONUS_RATIO = 0.0
-
-NDefines.NMilitary.BASE_DIVISION_BRIGADE_GROUP_COST = 0
-NDefines.NMilitary.BASE_DIVISION_BRIGADE_CHANGE_COST = 0
-NDefines.NMilitary.BASE_DIVISION_SUPPORT_SLOT_COST = 0
-
-NDefines.NNavy.NAVAL_SUPREMACY_CAN_INVADE = -1.0
-NDefines.NIntel.NAVAL_SUPREMACY_INTEL_LOW = -1.0
-
-NDefines.NAI.MIN_INVASION_PLAN_VALUE_TO_EXECUTE = 0
-
-NDefines.NAir.STRATEGIC_BOMBER_NUKE_AIR_SUPERIORITY = 0.5
-NDefines.NCountry.NUCLEAR_BOMB_DROP_WAR_SUPPORT_EFFECT_MAX_VP = 10
-NDefines.NCountry.NUCLEAR_BOMB_DROP_WAR_SUPPORT_EFFECT_MAX_INFRA = 0.05
-
-NDefines.NResistance.RESISTANCE_GROWTH_BASE = 0.1       --Vanilla: 0.2
-NDefines.NResistance.COMPLIANCE_DECAY_AT_MAX_COMPLIANCE = 0.0
-
 NDefines.NCountry.BASE_RESEARCH_SLOTS = 1       --Vanilla: 2
 
-NDefines.NProduction.MAX_EQUIPMENT_RESOURCES_NEED = 5       --Vanilla: 3
+--NDefines.NProduction.MAX_EQUIPMENT_RESOURCES_NEED = 5       --Vanilla: 3
--- common/defines/mod_graphics.lua
+++ common/defines/mod_graphics.lua
@@ -80,5 +80,5 @@ NDefines_Graphics.NGraphics.RAILWAY_BRIDGE_GAP_LARGE_WIDTH = 0.0			-- Railways w
 NDefines_Graphics.NGraphics.TRAIN_MAP_SPEED = 4.0							-- Trains will move at this relative speed. This has no gameplay implications. Changing this value (originally 4.0) may cause audio effects to lose sync with animation.
 NDefines_Graphics.NGraphics.TUNNELBANA_TIMETABLE = { 4000, 6000 }
 
-NDefines_Graphics.NInterface.MIN_FOCUS_TREE_ZOOM = 0.5
-NDefines_Graphics.NInterface.MAX_FOCUS_TREE_ZOOM = 1.0
+--NDefines_Graphics.NInterface.MIN_FOCUS_TREE_ZOOM = 0.5
+--NDefines_Graphics.NInterface.MAX_FOCUS_TREE_ZOOM = 1.0
