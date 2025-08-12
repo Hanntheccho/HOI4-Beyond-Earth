c508ce9f1e8c24b4f036f58c4575b61127ef49b9
Hanntheccho <jmgp0411@gmail.com>
2025-08-12 11:52:45 +0200

    checkpoint commit

--- common/defines/mod_graphics.lua
+++ common/defines/mod_graphics.lua
@@ -40,15 +40,6 @@ NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_STATE_B = 1.0
 NDefines_Graphics.NGraphics.POLITICAL_GRID_SMALL_BOX_LIMIT = 100    -- Vanilla: 6
 NDefines_Graphics.NGraphics.SETUP_SPIRIT_GRID_BOX_LIMIT = 100   -- Vanilla: 3
 
-NDefines_Graphics.NGraphics.FRIEND_COLOR = {1.0, 1.0, 1.0}
-NDefines_Graphics.NGraphics.ENEMY_COLOR = {1.5, 0.9, 0.6}
-NDefines_Graphics.NGraphics.NEUTRAL_COLOR = {1.1, 1.1, 0.8}
-
-NDefines_Graphics.NGraphics.COUNTER_MODE_ALLEGIANCE_OURS = { 0.7, 0.6, 1.25, 1.0 }
-NDefines_Graphics.NGraphics.COUNTER_MODE_ALLEGIANCE_ALLIED = { 0.2, 0.8, 1.5, 0.6 }
-NDefines_Graphics.NGraphics.COUNTER_MODE_ALLEGIANCE_ENEMY = { 1.2, 0.4, 0.3, 1.0 }
-NDefines_Graphics.NGraphics.COUNTER_MODE_ALLEGIANCE_OTHER = { 0.8, 0.8, 0.8, 0.6 }
-
 NDefines_Graphics.NGraphics.RAILWAY_CAMERA_CUTOFF = 2000.0
 NDefines_Graphics.NGraphics.RAILWAY_CAMERA_CUTOFF_SPEED = 1.0
 
